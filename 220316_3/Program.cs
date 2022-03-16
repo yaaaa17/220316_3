a4c9b4b6eb1a8e47f1ce69a593be6073df9e4d6a
candy <C110196104@nkust.edu.tw>
2022-03-16 16:57:42 +0800

    加入專案檔案。

---
+++ 220316_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220316_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            double c = 999D; //設定絕對不可能出現的數
+            char op; //加減乘除只有一個(char)字元
+
+            Console.Write("Number1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number2:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Operator(+, -, *, /):");
+            op = Convert.ToChar(Console.ReadLine());
+
+            switch(op)
+            {
+                case '+':
+                    c = Convert.ToDouble(a + b);
+                    break;
+                case '-':
+                    c = Convert.ToDouble(a - b);
+                    break;
+                case '*':
+                    c = Convert.ToDouble(a * b);
+                    break;
+                case '/':
+                    c = Convert.ToDouble(a) / b; //改成double->取得浮點數
+                    break;
+                default:
+                    Console.WriteLine("無法執行你要求的運算。");
+                    break;
+            }
+            if (c != 999D) :
+                    Console.WriteLine("計算結果：{0}", c);
+            Console.ReadLine();
+        }
+    }
+}
